57b1c95cf99d8da55b57ab343a5c4d7c7673cd9a
tge0rgieva <tanya.georgieva@hotmail.com>
2020-07-14 11:57:54 +0300

    Add mysql connection config

--- atomic_tracker/settings.py
+++ atomic_tracker/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'atomic_tracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sakila',
+        'USER': 'django_test',
+        'PASSWORD': 'django_test_password',
+        'OPTIONS': {
+          'autocommit': True
+        },
     }
 }
 
